3877f0dda14695b504c3e858ad7e46e040fe4737
Ariel Rivero <jrivero@hipotecario.com.ar>
2025-04-09 00:13:59 -0300

    Agrego servicio consumer

---
+++ consumer/src/test/java/ar/com/apache_kafka/consumer/service/Consumer.java
@@ -0,0 +1,18 @@
+package ar.com.apache_kafka.consumer.service;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Consumer {
+
+	private static final Logger log = LogManager.getLogger(Consumer.class);
+	
+	@KafkaListener(topics = "test-topic", groupId = "my-group")
+	public void listener(String message) {
+		System.out.println("El mensaje es: " + message);
+		log.info("Mensaje escuchado {}", message);
+	}
+}
