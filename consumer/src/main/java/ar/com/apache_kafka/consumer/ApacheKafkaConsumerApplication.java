37159e3bdeb3325bfd92031da5c3cf60979ac975
Ariel Rivero <jrivero@hipotecario.com.ar>
2025-04-10 19:13:07 -0300

    Agrego clase de configuración para consumer

--- consumer/src/main/java/ar/com/apache_kafka/consumer/ApacheKafkaConsumerApplication.java
+++ consumer/src/main/java/ar/com/apache_kafka/consumer/ApacheKafkaConsumerApplication.java
@@ -2,8 +2,10 @@ package ar.com.apache_kafka.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class ApacheKafkaConsumerApplication {
 
 	public static void main(String[] args) {
